3d5b60e495ae36207d384f712f6b45cf3b02dfbe
btk123-sss <bunthern.kim9@gmail.com>
2026-09-22 20:07:18 +0700

    Auto-commit: 2026-09-22 20:07:18

--- c3_recurse1.cpp
+++ c3_recurse1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
 // Factorial function using Loop (iteration)
@@ -17,10 +18,10 @@ int factorialLoop(int n){
 }
 
 // using recursion
-// n! = f(n)   = n (n-1)! = n*f(n-1) 
+// n! = f(n) = n (n-1)! = n*f(n-1) 
 int factorialRecursion(int n){
+    // cout << "running recursion" << endl;
     // base case 
-    cout << "running recursion" << endl;
     if (n==0) return 1;
     else return n*factorialRecursion(n-1);
 }
@@ -32,18 +33,55 @@ int sumArray(int* A, int n){
 }
 
 
+long long Fib(int n){
+    if (n <= 1){
+        return n;
+    }
+    else {
+        return Fib(n-1) + Fib(n-2);
+    }
+}
+// Space ->  O(?)  -> Stack memory -> O(n)
+// Runtime -> O(?)  -> O(2^n)   (Exact:  (1.618)^n)
+
+map<int, long long> cache;
+long long FibWithMemorization(int n){
+    if (n <= 1){
+        return n;
+    }
+    if (n <= cache.size()) return cache[n];
+    else {
+        cache[n] = FibWithMemorization(n-1) + FibWithMemorization(n-2);
+        return cache[n];
+    }
+} // Runtime -> O(n) 
+
+
 int main(){
 
-    int Score[5] = {33, 45, 55, 66 , 77};
-    int n = 5;
-    int sum = sumArray(Score, n);
-    cout << sum << endl;
-    
+    // int result = factorialLoop(10);
+    // cout << result << endl;
 
-    int result = factorialLoop(10);
-    cout << result << endl;
+    // int result2 = factorialRecursion(10);
+    // cout << result2 << endl;
 
-    int result2 = factorialRecursion(10);
-    cout << result2 << endl;
+    // int Score[5] = {33, 45, 55, 66 , 77};
+    // int n = 5;
+    // int sum = sumArray(Score, n);
+    // cout << sum << endl;
+
+    // Long running code 
+    // cout << Fib(50) << endl;
+
+    // Fast running code 
+    cout << FibWithMemorization(50) << endl;
+
+
+    // map<int, int> m;
+    // m[0] = 10;
+    // m[1] = 20;
+    // cout << m.size() << endl;
+
+
     return 0;
 }
